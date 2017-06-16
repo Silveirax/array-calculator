77fdf6ed73991f3edc6845e06ac96965b80dfea9
Jessica Cacau <jessicaccp@users.noreply.github.com>
2017-06-16 12:58:05 -0300

    Update trabalho.c

--- trabalho.c
+++ trabalho.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include "vetores.h"
 #include "matrizes.h"
-#include "menu.h"
+//#include "menu.h"
 
 int main () {
-    menu();
+    //menu();
 
-   /* receber_vetor();
+    receber_vetor();
     receber_vetor();
     receber_vetor();
     exibir_vetores();
     soma_vetor(inicio_v, inicio_v->prox);
     multiplica_vetor(inicio_v, 10);
     produto_escalar(inicio_v, inicio_v->prox);
-    somageral_vetor(3);*/
+    somageral_vetor(3);
 
 
 /*    receber_matriz();
